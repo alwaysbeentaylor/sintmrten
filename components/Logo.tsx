c6bda5fb06f8b951864fe9231fbcb5eda30cb57f
alwaysbeentaylor <table.23@icloud.com>
2025-11-07 20:48:52 +0100

    Replace local logo with external logo from CDN

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -6,10 +6,10 @@ export default function SintMaartenLogo({ className = 'w-16 h-16' }: { className
   return (
     <div className={`relative ${className}`}>
       <Image
-        src="/logo.svg"
+        src="https://cdn.prod.website-files.com/62fbeeefbab5395307ae17a3/62fbef608337990b58b105e1_vbs-p-500.webp"
         alt="Sint Maarten Logo"
-        width={64}
-        height={64}
+        width={200}
+        height={200}
         className="w-full h-full object-contain"
         priority
       />
